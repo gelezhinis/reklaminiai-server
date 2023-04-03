ebe51afbe56fe716f3e367b77b8192daeea6efca
Linas Geležauskas <gelezhinis@gmail.com>
2023-04-03 15:56:45 +0300

    second

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('reklaminiai', 'root', 'root', {
   dialect: 'mysql',
-  host: 'localhost',
+  host: '194.31.55.158',
   dialectModule: require('mysql2')
 });
 
