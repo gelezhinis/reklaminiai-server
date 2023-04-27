b0d5c3471e7eb3c31815dd1437590f6e4d2a5caf
Linas Geležauskas <gelezhinis@gmail.com>
2023-04-27 16:52:56 +0300

    fixed mysql configuration

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,10 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('reklaminiai', 'root', 'root', {
+const sequelize = new Sequelize('reklaminiai', 'root', 'meidaNatas200', {
   dialect: 'mysql',
-  // host: '194.31.55.158',
-  host: 'localhost',
+  host: '194.31.55.158',
+  // host: 'localhost',
+  port: '/var/run/mysqld/mysqld.sock',
   dialectModule: require('mysql2')
 });
 
