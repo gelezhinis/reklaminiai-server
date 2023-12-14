7ac0ce2001966227a985d8919a303e68af3700e0
Linas <44196223+gelezhinis@users.noreply.github.com>
2023-12-14 15:45:55 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('reklaminiai', 'root', process.env.DB_PASSWORD, {
-// const sequelize = new Sequelize('reklaminiai', 'root', 'root', {
   dialect: 'mysql',
   host: process.env.DB_HOST,
-  // host: 'localhost',
   // port: '/var/run/mysqld/mysqld.sock',
   dialectModule: require('mysql2')
 });
